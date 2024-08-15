bf33275efdceb22f995566c881e723be70bd58eb
jovitorRdm <jovitorrdm@gmail.com>
2024-08-14 21:42:47 -0300

    start-project

---
+++ src/pages/Home/Components/NewCycleForm/index.tsx
@@ -0,0 +1,45 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+import { useContext } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { CyclesContext } from '../../../../context/CyclesContext'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="">Vou trabalhar em</label>
+
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="projeto01"></option>
+        <option value="projeto02"></option>
+        <option value="projeto03"></option>
+        <option value="banana"></option>
+      </datalist>
+
+      <label htmlFor="">durante</label>
+
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        disabled={!!activeCycle}
+        step={5}
+        min={5}
+        max={60}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
